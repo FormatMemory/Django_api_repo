6844b31bd39bebb613858112d318d3519e9719cf
Yusheng Ding <davidthinkleding@Gmail.com>
2019-03-15 18:36:39 -0700

    superuser create func and format code

--- app/core/models.py
+++ app/core/models.py
@@ -2,17 +2,32 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """
         Creates and saves a new user
         """
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, passowrd=None, **extra_fields):
+        """
+        Creates and saves a new superuser
+        """
+        user = self.create_user(email, passowrd)
+        user.is_staff = True
+        user.is_superuser = True
+        user.user_type = "staff"
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
