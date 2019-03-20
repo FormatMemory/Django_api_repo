540611fdf0e7262e0a8b8408da4ee00253529ca8
Yusheng Ding <davidthinkleding@Gmail.com>
2019-03-20 12:14:06 -0700

    add secret key to .env and setting.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a4x2o9=)n*jg1@qv6l2o_&4yq)ym#rf#_c^d9i0w6zueuq+h)j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
