d51bc8a43ec2b1b49d086af77c8aa5f96c64b443
Yusheng Ding <davidthinkleding@Gmail.com>
2019-03-26 16:09:35 -0700

    change api doc. url

--- app/app/urls.py
+++ app/app/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls), 
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
-    url(r'^$', schema_view),
+    url(r'api/', schema_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
